e08e766e54375bc6e2987d59c2436627ba463623
Capril1042 <Caprilewis1042@gmail.com>
2019-03-10 11:15:46 -0400

    adds directions in README, and cleans up code

--- Cache.js
+++ Cache.js
@@ -6,49 +6,41 @@ var rl = readline.createInterface({
   terminal: false
 });
 
+/* This function counts number of repeating values in a map */
 function countRepetingValues( map, arg) {
     let count = 0;
-      for ( let value of map.values()) {
-                if (value === arg) {
-                    count++;
-                }
+    for ( let value of map.values()) {
+            if (value === arg) {
+                count++;
+            }
+        }
+    return count;
 }
-return count;
-}
+
 let memoryMap = new Map();
 let transactionMapArray = [];
 let numberOfOpenTransactions = 0;
 let commited = false;
 let helperNumber = 0;
+
 rl.on('line', function (line) {
     let commandInput = line.split(" ");
     let commandArgument1 = commandInput[0];
     let commandArgument2 = commandInput[1];
     let commandArgument3 = commandInput[2];
 
-    let openTransaction;
-    // insert condition that will check if input is valid
-    // it would be nice to also include a guide to valid commands
-
     switch(commandArgument1) {
         case "SET":
-        console.log(numberOfOpenTransactions);
-           if (numberOfOpenTransactions > 0 ) {
-               transactionMapArray[numberOfOpenTransactions-1].set(commandArgument2,commandArgument3);
+            if (numberOfOpenTransactions > 0 ) {
+                transactionMapArray[numberOfOpenTransactions-1].set(commandArgument2,commandArgument3);
                 helperNumber++;
-           } 
-           else {
-               memoryMap.set(commandArgument2,commandArgument3);
-           }
-           console.log(transactionMapArray);
-           console.log(transactionMapArray[numberOfOpenTransactions-1]);
-            console.log("\n");
+            } 
+            else {
+                memoryMap.set(commandArgument2,commandArgument3);
+            }
+                console.log("\n");
             break;
         case "GET":
-            console.log(memoryMap);
-            console.log(transactionMapArray);
-            console.log(commited);
-            console.log(numberOfOpenTransactions)
             let newMemoryMap = transactionMapArray[numberOfOpenTransactions-1];
             let value = (numberOfOpenTransactions > 0 && commited === false && helperNumber > 0) ? newMemoryMap.get(commandArgument2) : memoryMap.get(commandArgument2);
             let result = value ? value : "NULL";
@@ -62,11 +54,6 @@ rl.on('line', function (line) {
             else {
                 memoryMap.delete(commandArgument2);
             } 
-             console.log(memoryMap);
-            console.log(transactionMapArray);
-            console.log(commited);
-            console.log(numberOfOpenTransactions);
-            console.log(helperNumber);
             console.log("\n");
             break;
         case "NUMEQUALTO":
@@ -76,26 +63,17 @@ rl.on('line', function (line) {
             break;
         case "END":
             process.exit();
-            break; // is this break really needed if we are exiting the program?
         case "BEGIN":
             transactionMapArray.push( new Map() );
-            openTransaction = true;
             numberOfOpenTransactions++;
-            console.log(`DEBUG ${transactionMapArray}`);
-            console.log(transactionMapArray, numberOfOpenTransactions)
             console.log("\n");
             break;
-        case "ROLLBACK":
-            console.log(transactionMapArray);
+        case "ROLLBACK": 
             transactionMapArray.pop();
             numberOfOpenTransactions--;
             helperNumber--;
-            if (numberOfOpenTransactions === 0) {
-                openTransaction = false;
-                console.log("NO TRANSACTION");
-            }
-            console.log(`DEBUG ${transactionMapArray[numberOfOpenTransactions-1]}`)
-            console.log("\n");
+            let transaction = transactionMapArray.length === 0 && commited === true ? "NO TRANSACTION\n" : "\n";
+            console.log(transaction);
             break;
         case "COMMIT":
             if ( numberOfOpenTransactions > 0) {
@@ -103,15 +81,17 @@ rl.on('line', function (line) {
                for (let [key, value] of transactionMap.entries()) {
                  memoryMap.set(key,value);  
                } 
-               console.log(memoryMap);
                transactionMapArray.pop();
                numberOfOpenTransactions--;
                commited = true;
                console.log("\n")
             }
             else {
-                return  console.log("NO TRANSACTION");
+                return  console.log("NO TRANSACTION\n");
             }
+            break;
+        default:
+            console.log("*** PLEASE ENTER A VALID COMMAND ***");
             break;
 
     }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # In-Memory-Cache-Assignment
+
+## Steps to Invoke Cache
+ Step 1: clone the repository
+
+    ` git clone https://github.com/Capril1042/In-Memory-Cache-Assignment.git `
+
+Step 2:  move into the folder that you cloned 
+ 
+Step 3: run the program
+  ` node Cache.js`
+
+Step 4: begin typing commands
